af07f6de79184aaae9bdc6ab5b231e0e73cf3831
Himanshu Chuphal <himanshu.chuphal07@gmail.com>
2020-12-12 14:06:32 +0100

    Day1 python scripts
    Day1 python scripts + data

---
+++ 2020/day1_part2.py
@@ -0,0 +1,20 @@
+from itertools import *
+def main():
+    f= open("day1_data.txt","r") 
+    if f.mode == 'r':  
+        data =f.read()
+    raw = data.split("\n")
+    data_1 = [int(data) for data in raw]
+    pairs = list(combinations(data_1, 3))
+    ans_1 = list(filter(sums_2020, pairs))
+    print "Combination to 2020:: ", ans_1
+    product = 1
+    for x in ans_1[0]:
+        product *= x
+    print "Product::", product
+
+def sums_2020(values):
+    return sum(values) == 2020
+
+if __name__== "__main__":
+  main()
